c90d307a8e6f4ba39fb6d2bedd8ab1da5f9b7b9e
Diwash Sigdel <diwashsigdel13@gmail.com>
2019-08-12 17:23:12 -0400

    Hit the weather API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,70 @@
-import React, { Component } from 'react'
-import Title from './Components/Title'
+import React, { Component } from "react";
+import Title from "./Components/Title";
+import Form from "./Components/Form";
+import Weather from "./Components/Weather";
+
+const API_KEY = "0298ad3bdf17577eef5bc1160cd31e87";
 
 class App extends Component {
+  state = {
+    temperature: undefined,
+    maxTemp: undefined,
+    minTemp: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`
+    );
+    const data = await api_call.json();
+    console.log(data);
+    if( city && country) {
+      this.setState({
+        temperature: data.main.temp,
+        maxTemp: data.main.temp_max,
+        minTemp: data.main.temp_min,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ''
+      })
+    } else {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: 'Please enter desired City and Country to get the weather'
+        })
+    }
+  };
   render() {
     return (
       <div>
         <Title />
+        <Form getWeather={this.getWeather} />
+        <Weather 
+          temperature={this.state.temperature}
+          maxTemp={this.state.maxTemp}
+          minTemp={this.state.minTemp}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
