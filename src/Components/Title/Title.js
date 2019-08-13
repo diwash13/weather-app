a00df204a3565ac72a61b78d8cbf08461258fcc7
Diwash Sigdel <diwashsigdel13@gmail.com>
2019-08-13 15:12:49 -0400

    Add Bootstrap styling

---
+++ src/Components/Title/Title.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Title = () => (
+  <div>
+    <h1 className='title-container__title'>Weather Forecast</h1>
+    <p className='title-container__subtitle'>Your reliable source for weather...</p>
+  </div>
+);
+
+export default Title;
